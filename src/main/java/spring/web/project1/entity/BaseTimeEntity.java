789b9a71b7f3c76f26de5d651aa79578d0d8a959
hichocolatemilk <111757770+hichocolatemilk@users.noreply.github.com>
2023-05-30 22:53:21 +0900

    시간 포맷 변경

--- src/main/java/spring/web/project1/entity/BaseTimeEntity.java
+++ src/main/java/spring/web/project1/entity/BaseTimeEntity.java
@@ -1,12 +1,16 @@
 package spring.web.project1.entity;
 
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 @Getter
@@ -14,7 +18,21 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
+    //createDate를 설정해야 dataformat 제대로 됨
+    @CreatedDate
+    private String createdDate;
 
     @LastModifiedDate
-    private LocalDateTime modifiedDate;
+    private String modifiedDate;
+
+    @PrePersist
+    public void onPrePersist(){
+        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        this.modifiedDate = this.createdDate;
+    }
+
+    @PreUpdate
+    public void onPreUpdate(){
+        this.modifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    }
 }
